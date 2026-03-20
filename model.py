7b17f13039b43b8ccb0933fe35363af6443667d6
Muffy53 <muffydore53@gmail.com>
2026-03-20 21:40:37 +0530

    Encoding the data numbers

--- model.py
+++ model.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # --- SECTION 1: LOAD DATA ---
-# This part belongs to Member 1 (Lead)
 df = pd.read_csv('insurance_data_linear.csv')
 
 print("Dataset loaded successfully!")
 print(f"Shape of dataset: {df.shape}")
 print(df.head())
 
+
+
+
+# Member 3: Converting words to numbers (Encoding)
+df = pd.get_dummies(df, columns=['sex', 'smoker', 'region'], drop_first=True)
+print("Categorical variables encoded")
