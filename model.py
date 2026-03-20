7cee84d3da2b8d3edcb3d20eeef3faf286dde4bb
Muffy53 <muffydore53@gmail.com>
2026-03-20 14:48:52 +0530

    Initial Data Loading By Lead

--- model.py
+++ model.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# --- SECTION 1: LOAD DATA --
+df = pd.read_csv('insurance_data_linear.csv')
+
+print("Dataset loaded successfully!")
+print(f"Shape of dataset: {df.shape}")
+print(df.head())
+
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Load dataset
